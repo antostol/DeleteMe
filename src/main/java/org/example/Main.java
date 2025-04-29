f1a5d1a2fb510e5da6c5a48d4e8d3bd1c07534e0
Antonia Stoleru <2484936@edu.vaniercollege.qc.ca>
2025-04-29 15:03:54 -0400

    add SelectionSorting method in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {3, 1, 5, 2, 1, 4};
 
-        bubbleSort(nums); // {1, 1, 2, 3, 4, 5}
+        SelectionSort(nums); // {1, 1, 2, 3, 4, 5}
 
         System.out.println(Arrays.toString(nums));
     }
@@ -33,7 +33,7 @@ public class Main {
     * 11234 does it until all the numbers are on the side
     *
     * BubbleSort contains 2 loops => nested loop*/
-// this method is Big O(n^2)
+    // this method is Big O(n^2)
     public static void bubbleSort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - 1 - i; j++) {
@@ -43,6 +43,29 @@ public class Main {
                    nums[j + 1] = temp;
                }
             }
+        }
+    }
+
+    /*
+    * 315214 considers the first element as the smallest one and changes its value as it goes through the elements => starts with 3 ends with 1
+    * 135214 3 ends with the second 1
+    * 113524 3 ends with 2
+    * 112354 3 ends with 3
+    * 112354 5 ends with 4
+    * 112345
+    * 2 loops => nested for loop*/
+
+    public static void SelectionSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIdx];
+            nums[minIdx] = temp;
         }
     }
 }
